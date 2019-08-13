f33eed9800fceaf359f6c5ea1886b3ebe3628b12
Luciano Pereira <luciano.pereira@w3as.com>
2019-08-13 11:16:49 -0300

    ajuste nos artigos em ingles

--- config/portfolio/EN.js
+++ config/portfolio/EN.js
@@ -2,11 +2,14 @@ export default {
   sourcererIO: 'https://sourcerer.io/lucianopereira86',
   articles: [{
     site: 'Vila do Silício',
-    urls: [{
+    links: [{
+      title: 'Social network authentication with Quasar, Cordova and Hello.js',
       url: 'http://viladosilicio.com.br/autenticacao-em-redes-sociais-com-quasar-cordova-e-hello-js/'
     }, {
+      title: 'Upload and download files from Google Drive with Quasar and NodeJS',
       url: 'http://viladosilicio.com.br/upload-e-download-de-arquivos-do-google-drive-com-quasar-e-nodejs/'
     }, {
+      title: 'Text extraction from images with Google Vision, Quasar and NodeJS',
       url: 'http://viladosilicio.com.br/extracao-de-texto-de-imagens-com-google-vision-quasar-e-nodejs/'
     }]
   }],
